420ebbb5447127777fa43d60701a5987326591c6
CWLi-freedom <abramlili2@gmail.com>
2021-04-06 15:53:29 +0800

    work for color flipper

--- 1-color-flipper/setup/app.js
+++ 1-color-flipper/setup/app.js
@@ -1,1 +1,18 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.getElementById("btn");
+const backgroundcolor1 = document.querySelector(".backgroundcolor1");
+
+btn.addEventListener('click', function()
+{
+    //get the randon number and change the background color
+    var randomNumber = GetRandomNumber();
+    console.log(randomNumber);
+    document.body.style.backgroundColor = colors[randomNumber];
+    backgroundcolor1.textContent = colors[randomNumber];
+})
+
+function GetRandomNumber()
+{
+    return Math.floor(Math.random() * colors.length);
+}
--- 1-color-flipper/setup/hex.js
+++ 1-color-flipper/setup/hex.js
@@ -1,1 +1,24 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+document.addEventListener('click', function()
+{
+    //gen the hex color to background color
+    let hexcolor = '#';
+    for (let i = 0; i < 6; i++)
+    {
+        let genedNum = GetRandomHexValue();
+        hexcolor += hex[genedNum];
+        console.log(genedNum);
+    }
+    
+    color.textContent = hexcolor;
+    document.body.style.backgroundColor = hexcolor;
+})
+
+function GetRandomHexValue()
+{
+    return Math.floor(Math.random() * hex.length);
+}
