24547cc1ca9eb99ca0f94de544f43f2e1c9018cf
CWLi-freedom <abramlili2@gmail.com>
2021-04-11 20:10:30 +0800

    question proj with  traversing the dom

--- 7-questions/setup/app.js
+++ 7-questions/setup/app.js
@@ -1,2 +1,34 @@
 //using selectors inside the element
 // traversing the dom
+
+//.show-text 
+// const btns = document.querySelectorAll('.question-btn');
+
+// btns.forEach(function(btn)
+// {
+//     btn.addEventListener('click', function(e)
+//     {
+//         const question = e.currentTarget.parentElement.parentElement;
+//         question.classList.toggle('show-text');
+
+//     });
+// });
+
+const questions = document.querySelectorAll('.question');
+questions.forEach(function(orange)
+    {
+        const questionBtn = orange.querySelector('.question-btn');
+        questionBtn.addEventListener('click', function()
+        {
+
+            questions.forEach(function(item)
+            {
+                if (item !== orange)
+                {
+                    item.classList.remove('show-text');
+                }
+            });
+            orange.classList.toggle('show-text');
+        })
+
+    });
